677bae2d788f2e0456e80a4cdeb33d160bd8b4c5
Abhinav K <abhinavksathyann@gmail.com>
2025-10-28 20:59:15 +0530

    new file:   student/register.php

---
+++ student/register.php
@@ -0,0 +1,23 @@
+<?php
+include '../config/db.php';
+
+if(isset($_POST['register'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $pass = password_hash($_POST['password'], PASSWORD_BCRYPT);
+    $role = 'student';
+
+    $sql = "INSERT INTO users(name, email, password, role) VALUES('$name','$email','$pass','$role')";
+    if(mysqli_query($conn, $sql)){
+        echo "<script>alert('Registration Successful!'); window.location='login.php';</script>";
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
+}
+?>
+<form method="post">
+  <input type="text" name="name" placeholder="Full Name" required><br>
+  <input type="email" name="email" placeholder="Email" required><br>
+  <input type="password" name="password" placeholder="Password" required><br>
+  <button type="submit" name="register">Register</button>
+</form>
